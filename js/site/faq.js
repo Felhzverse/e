5a7e3f322ed8a0d28a8303511b97f7c8904cf699
Peppii <nick.delbaggio@gmail.com>
2019-01-09 22:03:31 -0500

    Trying to do buttons for FAQ.

--- js/site/faq.js
+++ js/site/faq.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+
+function qBlock1() {
+	var x = document.getElementById ('qBlock1')
+	 if (x.style.display === "none"){
+		document.getElementById.style.display = "show" 
+}	else {
+	x.style.display = "none"
+}
