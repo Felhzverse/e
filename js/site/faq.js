39023de5fd89ce0f516ee16aeec7910968dab166
Amina Webster <duckmobile9614@gmail.com>
2019-01-10 22:56:39 -0500

    formatting edits

--- js/site/faq.js
+++ js/site/faq.js
@@ -1,9 +1,11 @@
 // JavaScript Document
 
-function qBlock1() {
-	var x = document.getElementById ('qBlock1')
-	 if (x.style.display === "none"){
-		document.getElementById.style.display = "show" 
-}	else {
-	x.style.display = "none"
+function qB1() {
+var x = document.getElementById ('qB1');
+if (x.style.display === "none"){
+		document.getElementById.style.display = "show" ;
+}
+   else {
+	x.style.display = "none";
+}
 }
