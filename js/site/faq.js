7b8d0d39a210c2a43da37da23a07a4008e3bb5a9
Amina Webster <duckmobile9614@gmail.com>
2019-01-10 23:09:10 -0500

    about complete

--- js/site/faq.js
+++ js/site/faq.js
@@ -1,11 +1,66 @@
 // JavaScript Document
 
 function qB1() {
-var x = document.getElementById ('qB1');
-if (x.style.display === "none"){
-		document.getElementById.style.display = "show" ;
+  var a = document.getElementById("aB1");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
 }
-   else {
-	x.style.display = "none";
+function qB2() {
+  var a = document.getElementById("aB2");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
 }
+function qB3() {
+  var a = document.getElementById("aB3");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB4() {
+  var a = document.getElementById("aB4");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB5() {
+  var a = document.getElementById("aB5");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB6() {
+  var a = document.getElementById("aB6");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB7() {
+  var a = document.getElementById("aB7");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB8() {
+  var a = document.getElementById("aB8");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
 }
