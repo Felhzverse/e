4810b3a5e4addae20fc9c33f612514d88a1f6784
Amina Webster <duckmobile9614@gmail.com>
2019-02-12 22:34:36 -0500

    char index b start

--- js/site/faq.js
+++ js/site/faq.js
@@ -64,3 +64,99 @@ function qB8() {
     a.style.display = "block";
   }
 }
+function qB9() {
+  var a = document.getElementById("aB9");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB10() {
+  var a = document.getElementById("aB10");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB11() {
+  var a = document.getElementById("aB11");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB12() {
+  var a = document.getElementById("aB12");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB13() {
+  var a = document.getElementById("aB13");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB14() {
+  var a = document.getElementById("aB14");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB15() {
+  var a = document.getElementById("aB15");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB16() {
+  var a = document.getElementById("aB16");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB17() {
+  var a = document.getElementById("aB17");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB18() {
+  var a = document.getElementById("aB18");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB19() {
+  var a = document.getElementById("aB19");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+function qB20() {
+  var a = document.getElementById("aB20");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
