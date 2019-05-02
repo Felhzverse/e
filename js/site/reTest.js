0be83172588d4055cd3f4ba1f1c5b9020c682e2b
Amina Webster <duckmobile9614@gmail.com>
2019-05-02 19:11:09 -0400

    new font, fixed navbar, updated main pages, new css, redirect

---
+++ js/site/reTest.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+function redirect(){
+"use strict"; 
+	var url = window.location.pathname;
+	var x = url.length;
+	var y= x-5; 
+	var z= url.substring(0,y);
+	if(url.substring(0,18)==="/C:/Users/prett/e/"){
+	  "fail";
+	   }
+	else{
+		window.location.replace(z);
+
+	   }
+}
